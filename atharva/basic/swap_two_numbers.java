251ded78db1c3ef48ea87e95320543796e78bcad
atharvabhagwat007 <bhagwatatharva50@gmail.com>
2023-03-14 16:37:40 +0530

    swap two numbers

---
+++ atharva/basic/swap_two_numbers.java
@@ -0,0 +1,14 @@
+package atharva.basic;
+
+public class swap_two_numbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int temp = a;
+         a = b;
+        b= temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
