1e63e44a7e167b83eddc15ab884f921773cffd5b
atharvabhagwat007 <bhagwatatharva50@gmail.com>
2023-03-15 17:36:17 +0530

    largest smallest number

---
+++ atharva/basic/largest_smallest.java
@@ -0,0 +1,27 @@
+package atharva.basic;
+
+import java.util.Arrays;
+
+public class largest_smallest {
+    public static void main(String[] args) {
+        int[] arr = {5,4,2,6,8,-1};
+
+        System.out.println(Arrays.toString(largestSmallest(arr)));
+    }
+
+    static int[] largestSmallest(int[] arr)
+    {
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for (int j : arr) {
+            if (j > largest) {
+                largest = j;
+            }
+            if (j < smallest) {
+                smallest = j;
+            }
+        }
+        return new int[]{largest,smallest};
+    }
+}
