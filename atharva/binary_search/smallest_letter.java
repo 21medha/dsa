5e64d99f980fe41271540b5cae33576e2e5c0104
atharvabhagwat007 <bhagwatatharva50@gmail.com>
2023-03-17 14:21:55 +0530

    smallest letter

---
+++ atharva/binary_search/smallest_letter.java
@@ -0,0 +1,40 @@
+package atharva.binary_search;
+
+public class smallest_letter {
+    public static void main(String[] args) {
+        char[] arr = {'c','f','j'};
+        char target = 'h';
+        System.out.println(smallestChar(arr,target));
+    }
+
+    static char smallestChar(char[] arr,char target)
+    {
+        int start = 0 ;
+        int end = arr.length - 1;
+
+        while( start <= end)
+        {
+            int mid  = (start + end)/2;
+
+            if(arr[mid] == target)
+            {
+                if(mid == arr.length - 1)
+                {
+                    return arr[0];
+                }
+                return arr[mid + 1];
+            }
+
+            if(arr[mid] > target)
+            {
+                end = mid - 1;
+            }
+            if(arr[mid]< target)
+            {
+                start = mid + 1;
+            }
+        }
+        return arr[0];
+
+    }
+}
